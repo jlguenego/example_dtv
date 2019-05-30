3f86d74ab6c1cd6a0be3a3f4fcc9a225c7354a4f
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-05-30 20:28:40 +0200

    ok

---
+++ 01-histogram/main.js
@@ -0,0 +1,17 @@
+(function () {
+    const svg = document.querySelector('svg');
+
+
+    var x = [];
+    for (var i = 0; i < 500; i++) {
+        x[i] = Math.random();
+    }
+
+    var trace = {
+        x: x,
+        type: 'histogram',
+    };
+    var data = [trace];
+    Plotly.newPlot('histo', data, {}, { showSendToCloud: true });
+
+})();
