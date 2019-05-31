a23b5545c4e07b8adb4520b242d49ce7ff65f108
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-05-31 14:03:01 +0200

    ok exemple 1

--- 01-histogram/main.js
+++ 01-histogram/main.js
@@ -1,17 +1,52 @@
 (function () {
-    const svg = document.querySelector('svg');
 
+    const MAX_VALUE = 500000;
 
-    var x = [];
-    for (var i = 0; i < 500; i++) {
-        x[i] = Math.random();
-    }
+    const plot = (n) => {
+        const histo = document.querySelector('#histo');
 
-    var trace = {
-        x: x,
-        type: 'histogram',
+        const x = [];
+        for (let i = 0; i < n; i++) {
+            x[i] = Math.random();
+        }
+
+        const trace = {
+            x: x,
+            type: 'histogram',
+            name: 'histogram about random function',
+            opacity: 0.2,
+            xbins: {
+                start: 0,
+                end: 1,
+                size: 0.1,
+            },
+            marker: {
+                color: 'hsla(240, 30%, 50%, 1)',
+                line: {
+                    width: 10,
+                    color: 'white',
+                }
+            },
+        };
+        const data = [trace];
+        const layout = {
+            title: 'Random distribution',
+            margin: { t: 150, l: 40, r: 40 },
+            font: { size: 18 }
+        };
+        Plotly.newPlot(histo, data, layout, { responsive: true });
+
+        document.querySelector('#samples').innerHTML = n;
     };
-    var data = [trace];
-    Plotly.newPlot('histo', data, {}, { showSendToCloud: true });
 
+
+
+    const range = document.querySelector('input[type="range"]');
+    const f = e => {
+        const value = Math.round(Math.exp((Math.log(MAX_VALUE) / 100) * e.target.value));
+        plot(value);
+    };
+
+    range.addEventListener('input', f);
+    f({ target: range });
 })();
