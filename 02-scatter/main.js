137ecc4dcde1fd844e58be25ef1f6a60e0d6da54
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-05-31 14:47:18 +0200

    ok

---
+++ 02-scatter/main.js
@@ -0,0 +1,54 @@
+(function () {
+
+    const MAX_VALUE = 500000;
+    const START_VALUE = 500;
+
+    const plot = (n) => {
+        const histo = document.querySelector('#histo');
+
+        const x = [];
+        for (let i = 0; i < n; i++) {
+            x[i] = Math.random();
+        }
+
+        const trace = {
+            x: x,
+            type: 'histogram',
+            name: 'histogram about random function',
+            opacity: 0.2,
+            xbins: {
+                start: 0,
+                end: 1,
+                size: 0.1,
+            },
+            marker: {
+                color: 'hsla(240, 30%, 50%, 1)',
+                // line: {
+                //     width: 10,
+                //     color: 'white',
+                // }
+            },
+        };
+        const data = [trace];
+        const layout = {
+            title: 'Random distribution',
+            margin: { t: 150, l: 40, r: 40 },
+            font: { size: 18 }
+        };
+        Plotly.newPlot(histo, data, layout, { responsive: true });
+
+        document.querySelector('#samples').innerHTML = n;
+    };
+
+
+
+    const range = document.querySelector('input[type="range"]');
+    range.value = Math.round(Math.log(START_VALUE) / (Math.log(MAX_VALUE) / 100));
+    const f = e => {
+        const value = Math.round(Math.exp((Math.log(MAX_VALUE) / 100) * e.target.value));
+        plot(value);
+    };
+
+    range.addEventListener('input', f);
+    f({ target: range });
+})();
