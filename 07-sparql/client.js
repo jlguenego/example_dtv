bc6432a3dd9f746c0d7acbefefe8d09da4b2c896
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-06-08 20:57:02 +0200

    ok

--- 07-sparql/client.js
+++ 07-sparql/client.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const fs = require('fs');
 const endPoint = 'https://query.wikidata.org/bigdata/namespace/wdq/sparql';
 
-const sparql = encodeURIComponent(fs.readFileSync('./department.sparql'));
+const sparql = encodeURIComponent(fs.readFileSync('./department.sparql.txt'));
 console.log('sparql', sparql);
 
 // Make a request for a user with a given ID
