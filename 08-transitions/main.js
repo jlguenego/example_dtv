bdebd202870c8c285a73e381f216cec671cd88cc
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-06-15 20:01:59 +0200

    ok

--- 08-transitions/main.js
+++ 08-transitions/main.js
@@ -1,8 +1,4 @@
 (async function () {
-    console.log('starting');
-    console.log('d3', d3);
-
-
     const scale = 2;
 
     const wikidataUrl = 'https://query.wikidata.org/bigdata/namespace/wdq/sparql';
@@ -22,12 +18,32 @@ ORDER BY DESC(?superficie)
     const data = await d3.sparql(wikidataUrl, request);
     console.log('data', data);
 
-    const root = d3.select('div.root');
-    const item = root.selectAll('div.items')
-        .data(['hello', 'coucou', 'titi'])
-        .enter()
-        .append('div')
-        .classed("item", true)
-        .text(d => d)
-        .style("transform", (d, i) => `translate(0, ${i * scale}em)`);
+    const draw = (data) => {
+        const root = d3.select('div.root');
+        const item = root.selectAll('div.items')
+            .data(data)
+            .enter()
+            .append('div')
+            .classed("item", true)
+            .html(d => `<div>${d.riverLabel}</div><div>${d.longueur}</div><div>${d.superficie}</div>`)
+            .style("transform", (d, i) => `translate(0, ${i * scale}em)`);
+
+        
+    };
+
+    draw(data);
+
+    document.getElementsByName('classement').forEach(input => {
+        input.addEventListener('click', e => {
+            console.log('e', e);
+            const value = e.target.value;
+            console.log('value', value);
+            console.log('data', data);
+
+            const newData = data.sort((a, b) => a[value] < b[value]);
+            draw(newData);
+        });
+        
+    });
+    
 })();
