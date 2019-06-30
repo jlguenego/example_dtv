7e587488b0a4a4e703032c6c79f3b6e3bc930494
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-06-30 11:06:02 +0200

    accident histo

--- 10-histogramme-d3/main.js
+++ 10-histogramme-d3/main.js
@@ -1,6 +1,24 @@
 (async function () {
     'use strict';
 
+    const day = moment.weekdays().map(s => s.substr(0, 1).toUpperCase() + s.substr(1).toLowerCase());
+
+    function draw(data) {
+        const scale = 100 / 10000;
+        const histo = d3.select('#histo').selectAll('div.bar').data(data);
+
+        histo.enter().append('div').classed('bar', true)
+            .html((d, i) => `<span>${day[i]}</span>`)
+            .style('width', '0%');
+
+        histo.html((d, i) => `<span>${day[i]}</span><span>${d}</span>`)
+            .transition()
+            .duration(2000)
+            .ease(d3.easeSin)
+            .style('width', d => Math.floor(d * scale) + '%')
+            .style('background-color', d => `hsl(240, 30%, ${100 - d * scale / 2}%)`);
+    }
+
     async function buildData() {
         const pad = (a, b) => (1e15 + a + '').slice(-b);
         const data = await d3.csv('../09-accidents/caracteristiques-2017.csv');
@@ -14,19 +32,14 @@
     }
 
 
-
+    draw(new Array(7).fill(0));
     const data = await buildData();
     console.log('data', data);
-
-    const day = ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'];
-
+    draw(data);
 
 
-    const histo = d3.select('#histo');
-    console.log('histo', histo);
-    const scale = 100 / 10000;
-    histo.selectAll('div.bar').data(data).enter().append('div').classed('bar', true)
-        .style('width', d => Math.floor(d * scale) + '%')
-        .html((d, i) => `<span>${day[i]}</span><span>${d}</span>`);
+
+
+
 
 })();
