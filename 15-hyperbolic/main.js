ccdb20b2906bf6f297bc5649e092be9922964566
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-07-10 15:29:16 +0200

    scale integrated

--- 15-hyperbolic/main.js
+++ 15-hyperbolic/main.js
@@ -33,13 +33,21 @@
 
     const margin = ({ top: 0, right: 0, bottom: 0, left: 0 });
 
+    const x = d3.scaleLinear()
+        .domain([-10, 10])
+        .range([margin.left, width - margin.right]);
+
+    const y = d3.scaleLinear()
+        .domain([-10, 10])
+        .range([height - margin.bottom, margin.top]);
+
     const fx = t => Math.cos((2 * Math.PI / 6) * t + Math.PI / 6);
     const fy = t => Math.sin((2 * Math.PI / 6) * t + Math.PI / 6);
 
 
 
     const svg = d3.select('svg')
-        .attr('viewBox', [-width / 2, -height / 2, width, height]);
+        .attr('viewBox', [0, 0, width, height]);
     const circleGroup = svg.append('g').classed('circles', true);
     const pathGroup = svg.append('g').classed('paths', true);
 
@@ -52,37 +60,37 @@
         const u = buildU(side).map(p => [p[0] * a, p[1] * a]);
         console.log('u', u);
 
-        // const circles = circleGroup.selectAll('circle').data(u, function (d, i) {
-        //     return i;
-        // });
-        // circles.exit().remove();
-        // circles
-        //     .attr('cx', d => trans(d[0]))
-        //     .attr('cy', d => trans(d[1]))
-        //     .attr('r', 0.1);
-        // circles.enter().append('circle')
-        //     .attr('cx', d => trans(d[0]))
-        //     .attr('cy', d => trans(d[1]))
-        //     .attr('r', 0.1);
+        const circles = circleGroup.selectAll('circle').data(u, function (d, i) {
+            return i;
+        });
+        circles.exit().remove();
+        circles.transition().duration(1000)
+            .attr('cx', d => x(trans(d)[0]))
+            .attr('cy', d => y(trans(d)[1]))
+            .attr('r', 0.1);
+        circles.enter().append('circle')
+            .attr('cx', d => x(trans(d)[0]))
+            .attr('cy', d => y(trans(d)[1]))
+            .attr('r', 0.1);
 
 
         const paths = pathGroup.selectAll('path').data(u, function (d, i) {
             return i;
         });
         paths.exit().remove();
-        paths
-            .attr('d', d => d3.line()(range.map(t => trans([fx(t) + d[0], fy(t) + d[1]]))));
+        paths.transition().duration(1000)
+            .attr('d', d => d3.line()(range.map(t => trans([fx(t) + d[0], fy(t) + d[1]])).map(p => [x(p[0]), y(p[1])])));
         paths.enter().append('path')
-            .attr('d', d => d3.line()(range.map(t => trans([fx(t) + d[0], fy(t) + d[1]]))));
+            .attr('d', d => d3.line()(range.map(t => trans([fx(t) + d[0], fy(t) + d[1]])).map(p => [x(p[0]), y(p[1])])));
 
     };
 
     const euclide = p => p;
     const hyperbolic = p => {
-        const d = p.reduce((acc, x) => acc + x ** 2, 0)**0.5;
+        const d = p.reduce((acc, x) => acc + x ** 2, 0) ** 0.5;
         const A = 2.9;
         const B = 1;
-        const ratio = A/(d**0.95+B);
+        const ratio = A / (d ** 0.95 + B);
         return p.map(x => x * A * ratio);
     };
     let trans = hyperbolic;
