65616b341a2402c54e9ac74bf1efeea20c17cd75
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-07-10 12:30:20 +0200

    hyperbolic

--- 15-hyperbolic/main.js
+++ 15-hyperbolic/main.js
@@ -20,71 +20,79 @@
         // now add the point around the previous array.
         const length = 6 * side;
         const array = new Array(side).fill(0).map((n, i) => [side - i / 2, i * Math.sqrt(3) / 2]);
-        console.log('array', array);
 
         const hexagonArray = new Array(6).fill(0).reduce((acc, n, i) => {
             const a = rotateArray(array, i * Math.PI / 3);
-            console.log('a', a);
-
             return acc.concat(a);
         }, []);
-        console.log('hexagonArray', hexagonArray);
         return previous.concat(hexagonArray);
     };
-
-    const side = 5;
-    const u = buildU(side);
-    console.log('u', u);
 
     const width = 20;
     const height = 20;
 
     const margin = ({ top: 0, right: 0, bottom: 0, left: 0 });
 
-    const fx = t => Math.cos((2 * Math.PI / 6) * t);
-    const fy = t => Math.sin((2 * Math.PI / 6) * t);
-
-    const trans = d => 50 * d / (d + 1);
-
-
-
-    // const f1 = d3.line()(t.ticks(14).map(t => [x(fx(t)), y(fy(t))]));
-    // const f2 = d3.line()(t.ticks(14).map(t => [x(trans(fx(t))), y(trans(fy(t)))]));
-
-    // let fn = f1;
-
-    // const plot = svg => svg
-    //     .append('path')
-    //     .attr('d', fn);
+    const fx = t => Math.cos((2 * Math.PI / 6) * t + Math.PI / 6);
+    const fy = t => Math.sin((2 * Math.PI / 6) * t + Math.PI / 6);
 
 
 
     const svg = d3.select('svg')
         .attr('viewBox', [-width / 2, -height / 2, width, height]);
-
-    const circles = svg.append('g').classed('circles', true).selectAll('circle').data(u);
-    circles.enter().append('circle')
-        .attr('cx', d => d[0])
-        .attr('cy', d => d[1])
-        .attr('r', 0.1);
+    const circleGroup = svg.append('g').classed('circles', true);
+    const pathGroup = svg.append('g').classed('paths', true);
 
     const range = d3.scaleLinear().domain([0, 6]).ticks(7);
     console.log('range', range);
 
-    const paths = svg.append('g').classed('paths', true).selectAll('path').data(u);
-    paths.enter().append('path')
-        .attr('d', d3.line()(range.map(t => [fx(t), fy(t)])));
+    const draw = (side, trans) => {
 
-    // const plots = svg.append('g').classed('plots', true);
-    // plots.append('g').call(plot);
+        const a = Math.sqrt(3);
+        const u = buildU(side).map(p => [p[0] * a, p[1] * a]);
+        console.log('u', u);
+
+        // const circles = circleGroup.selectAll('circle').data(u, function (d, i) {
+        //     return i;
+        // });
+        // circles.exit().remove();
+        // circles
+        //     .attr('cx', d => trans(d[0]))
+        //     .attr('cy', d => trans(d[1]))
+        //     .attr('r', 0.1);
+        // circles.enter().append('circle')
+        //     .attr('cx', d => trans(d[0]))
+        //     .attr('cy', d => trans(d[1]))
+        //     .attr('r', 0.1);
 
 
+        const paths = pathGroup.selectAll('path').data(u, function (d, i) {
+            return i;
+        });
+        paths.exit().remove();
+        paths
+            .attr('d', d => d3.line()(range.map(t => trans([fx(t) + d[0], fy(t) + d[1]]))));
+        paths.enter().append('path')
+            .attr('d', d => d3.line()(range.map(t => trans([fx(t) + d[0], fy(t) + d[1]]))));
 
-    // document.querySelector('#transition').addEventListener('click', e => {
-    //     console.log('click');
-    //     fn = (fn === f1) ? f2 : f1;
-    //     plots.select('g').select('path')
-    //         .transition().duration(2000)
-    //         .attr('d', fn);
-    // });
+    };
+
+    const euclide = p => p;
+    const hyperbolic = p => {
+        const d = p.reduce((acc, x) => acc + x ** 2, 0)**0.5;
+        const A = 2.5;
+        const B = 1;
+        const ratio = A/(d**0.95+B);
+        return p.map(x => x * A * ratio);
+    };
+    let trans = hyperbolic;
+    const side = 10;
+    draw(side, trans);
+
+    document.querySelector('#transition').addEventListener('click', e => {
+        console.log('click');
+        trans = (trans === euclide) ? hyperbolic : euclide;
+        console.log('trans', trans);
+        draw(side, trans);
+    });
 })();
