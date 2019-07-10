175d625af51b2990fcd1a450cfeceec03dd735cd
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-07-10 17:21:58 +0200

    ok

--- 15-hyperbolic/main.js
+++ 15-hyperbolic/main.js
@@ -30,12 +30,12 @@
     const hive = buildHive(side).map(p => [p[0] * a, p[1] * a]);
     // console.log('hive', hive);
 
-    const draw = (hive, trans) => {
+    const draw = (hive, trans, duration) => {
         const circles = circleGroup.selectAll('circle').data(hive, function (d, i) {
             return i;
         });
         circles.exit().remove();
-        circles.transition().duration(1000)
+        circles.transition().duration(duration)
             .attr('cx', d => x(trans(d)[0]))
             .attr('cy', d => y(trans(d)[1]))
             .attr('r', 0.1);
@@ -48,7 +48,7 @@
             return i;
         });
         paths.exit().remove();
-        paths.transition().duration(1000)
+        paths.transition().duration(duration)
             .attr('d', d => d3.line()(range.map(t => trans([fx(t) + d[0], fy(t) + d[1]])).map(p => [x(p[0]), y(p[1])])));
         paths.enter().append('path')
             .attr('d', d => d3.line()(range.map(t => trans([fx(t) + d[0], fy(t) + d[1]])).map(p => [x(p[0]), y(p[1])])));
@@ -64,23 +64,34 @@
     };
     let trans = hyperbolic;
 
-    draw(hive, trans);
+    draw(hive, trans, 0);
 
     document.querySelector('#transition').addEventListener('click', e => {
         console.log('click');
         trans = (trans === euclide) ? hyperbolic : euclide;
         console.log('trans', trans);
-        draw(hive, trans);
+        draw(hive, trans, 1000);
     });
 
     // d3 zoom
     d3svg.call(d3.zoom().on('zoom', zoomed));
 
     function zoomed(...args) {
-        console.log('zoom', args, this, d3.event);
-        group.attr('transform', d3.event.transform);
-        
+        console.log('zoom', d3.event);
+        if (d3.event.sourceEvent instanceof WheelEvent) {
+            // group.attr('transform', d3.event.transform);
+            return;
+        }
+        const h = hive.map(p => {
+            const result = [
+                p[0] + d3.event.transform.x / 3,
+                p[1] - d3.event.transform.y / 3
+            ];
+            return result;
+        });
+        draw(h, trans, 0);
     }
+    
 
 
 })();
